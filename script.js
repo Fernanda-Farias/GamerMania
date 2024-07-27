29bd50451842e8a0c9d9189807569bc543c2ea4f
Fernanda Farias <contato.ffarias@gmail.com>
2024-07-27 12:37:29 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function login() {
+
+    var usuario = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+
+    if (usuario === '' || senha === '') {
+        document.getElementById('erro').innerText = 'Preencha os campos para efetuar o login';
+        document.getElementById('erro').style.color = 'red';
+        return;
+    }
+
+    // Aqui você pode adicionar a lógica de autenticação real, como enviar os dados para um servidor
+    if (usuario === 'Fernanda' && senha === '123') {
+        alert('Login feito com sucesso!');
+        // Redireciona para a página principal, por exemplo
+        window.location.href = 'index.html';
+    } else {
+        document.getElementById('erro').innerText = 'Usuário ou senha incorretos.';
+        document.getElementById('erro').style.color = 'red';
+    }
+}
